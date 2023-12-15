2c3782bde06fd30ecfe45e9fd743b271909ee7f4
jeonix <a01909144@aggies.usu.edu>
2023-12-15 11:34:13 -0700

    Changed remote to ssh

---
+++ app/navbar.jsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+const links = [
+    { href: "/", text: "Home", span: "home"},
+    { href: "/collection", text: "Collection", span: "collection"},
+    { href: "/upload", text: "Add", span: "upload"},
+];
+
+const Navbar = () => {
+    return (
+        <div className="container mx-auto mb-2 border-teal-300 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
+            <nav>
+                <ul className="flex justify-center py-5 rounded-sm bg-opacity-0">
+                    {links.map((l) => (
+                        <li className="pr-[2.5rem]" key={l.href}>
+                            <Link className="text-base text-neutral-300 hover:text-white" href={l.href}>
+                            {l.text}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
